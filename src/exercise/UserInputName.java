a76914e7e67fb6e19d62a3b41e567ed38fdf6090
anna-radnaeva <annaradnaeva@yahoo.com>
2022-02-01 10:48:34 -0600

    some

---
+++ src/exercise/UserInputName.java
@@ -0,0 +1,36 @@
+package exercise;
+
+import java.util.Scanner;
+
+
+public class UserInputName {
+		public static void main(String[] args) {
+			Scanner userInput=new Scanner(System.in);
+			
+			System.out.println("What is your name?");
+			String input=("My name is James Bond");
+			
+			System.out.println("How old are you?");
+			int age=userInput.nextInt();
+			System.out.println("oh, you are " +age+ " years old");
+			
+			System.out.println("What is your gender?");
+			
+					
+			System.out.println("Where do you live now?");
+			
+			
+			System.out.println("Where do you work?");
+			
+			
+			System.out.println("What is your job role?");
+			
+			
+			System.out.println("What is your salary?");
+			
+			
+			System.out.println("How much raise are you getting next year?");
+			
+		}
+
+}
---
+++ src/first/Hello.java
@@ -0,0 +1,54 @@
+package first;
+
+import java.util.Scanner;
+
+
+public class Hello {
+    public static void main(String[] args) {
+    	
+    	String name, gender, originalcity, currentcity, workplace, jobrole;
+    	int age;
+    	double salary, salaryraise;
+    	
+    	Scanner user=new Scanner (System.in);
+    	Scanner userInt=new Scanner (System.in);
+    	Scanner userDoubleInt=new Scanner (System.in);
+    	
+    	System.out.println("What is your name?");
+    	name=user.nextLine();
+    	
+    	System.out.println("How old are you?");
+    	age=userInt.nextInt();
+    	
+    	System.out.println("What is your gender?");
+    	gender=user.nextLine();
+    	
+    	System.out.println("Where are you from?");
+    	originalcity=user.nextLine();
+    	
+    	System.out.println("Where do you live now?");
+    	currentcity=user.nextLine();
+    	
+    	System.out.println("Where do you work now?");
+    	workplace=user.nextLine();
+    	
+    	System.out.println("What is your jobrole?");
+    	jobrole=user.nextLine();
+    	
+    	System.out.println("What is your salary?");
+    	salary=userDoubleInt.nextDouble();
+    	
+    	System.out.println("How much raise are you getting next year?");
+    	salaryraise=userDoubleInt.nextDouble();
+  
+    	System.out.print("My name is "+name+".");  
+    	System.out.print("I am "+age+" years old, "+gender+".");
+    	System.out.print("I work at "+workplace+" as an "+jobrole+".");
+    	System.out.print("I currently live in "+currentcity+".");
+    	System.out.print("I am originally from "+originalcity+".");
+    	System.out.print("I make "+salary+" dollars.");
+    	System.out.print("I will get a "+salaryraise+" raise next year so my total salary would be "+(salary+salaryraise)+".");
+    }
+    
+    
+}
